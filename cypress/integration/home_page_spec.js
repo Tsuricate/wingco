b18df02c1ed1288d57a1088a2800df6f145df6cf
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-02-03 22:18:45 +0100

    feature: write specs for homepage

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,19 @@
+import { URLS } from '../support/constants';
+
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit(URLS.HOME_PAGE);
+  });
+
+  it('provides user a link to new game page', () => {
+    cy.get(`a[href="${URLS.NEW_GAME_PAGE}"]`).should('be.visible');
+  });
+
+  it('provides user a link to join game page', () => {
+    cy.get(`a[href="${URLS.JOIN_GAME_PAGE}"]`).should('be.visible');
+  });
+
+  it('provides user a link to sign in', () => {
+    cy.get(`a[href="${URLS.SIGN_IN_PAGE}"]`).should('be.visible');
+  });
+});
