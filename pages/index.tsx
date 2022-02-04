f282f110c9c9539c9bae7d612503017daaaf5458
Tsukyko <zoeamat.pro@gmail.com>
2022-02-04 17:43:40 +0100

    Feature: home page title + description + links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { Heading, Stack, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +12,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <Stack spacing={{ base: 10 }}>
+          <Heading as="h1">Wingspan Companion</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, nostrum ipsam
+            repudiandae numquam libero temporibus magni ut, nihil explicabo minus quis quo. Modi
+            quod distinctio placeat ipsum consequatur dolorum maxime facere labore in vitae ipsam at
+            itaque, dolor dolorem ex facilis sapiente. Dolore facilis doloremque magnam soluta fugit
+            hic culpa.
+          </Text>
+          <Link href="/sign-in">Log in / Sign up</Link>
+          <Link href="/new-game">New game as guest</Link>
+          <Link href="/join-game">Join existing game</Link>
+        </Stack>
+      </main>
 
       <footer></footer>
     </div>
