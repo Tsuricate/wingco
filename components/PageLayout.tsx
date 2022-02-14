67cbf9516bed84bdfc277829330c03b5267e136d
Tsukyko <zoeamat.pro@gmail.com>
2022-02-14 14:16:47 +0100

    Refacto: Head component included in PageLayout

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,6 +1,21 @@
 import { Container } from '@chakra-ui/react';
+import Head from 'next/head';
 import React from 'react';
 
-const PageLayout: React.FC = ({ children }) => <Container as="main">{children}</Container>;
+interface PageLayoutProps {
+  title: string;
+}
+
+const PageLayout: React.FC<PageLayoutProps> = ({ title, children }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content="Wingspan Companion App" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Container as="main">{children}</Container>
+  </>
+);
 
 export default PageLayout;
