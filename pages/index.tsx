3765c81a18ddae69a878a942a8840fdefd0d5115
Tsukyko <zoeamat.pro@gmail.com>
2022-02-14 16:05:08 +0100

    i18n: finish Home page translation

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,16 @@ import Link from 'next/link';
 import PageLayout from '../components/PageLayout';
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation('home');
 
   return (
     <PageLayout title="WingCo">
       <Stack spacing={{ base: 10 }}>
-        <Heading as="h1">Wingspan Companion</Heading>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, nostrum ipsam repudiandae
-          numquam libero temporibus magni ut, nihil explicabo minus quis quo. Modi quod distinctio
-          placeat ipsum consequatur dolorum maxime facere labore in vitae ipsam at itaque, dolor
-          dolorem ex facilis sapiente. Dolore facilis doloremque magnam soluta fugit hic culpa.
-        </Text>
-        <Link href="/sign-in">Log in / Sign up</Link>
-        <Link href="/new-game">{t('guest')}</Link>
-        <Link href="/join-game">Join existing game</Link>
+        <Heading as="h1">{t('title')}</Heading>
+        <Text>{t('description')}</Text>
+        <Link href="/sign-in">{t('signIn')}</Link>
+        <Link href="/new-game">{t('newGameAsGuest')}</Link>
+        <Link href="/join-game">{t('joinGame')}</Link>
       </Stack>
     </PageLayout>
   );
@@ -30,6 +25,6 @@ export default Home;
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ['common'])),
+    ...(await serverSideTranslations(locale, ['home'])),
   },
 });
