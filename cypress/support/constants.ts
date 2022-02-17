3f459a74c8e9bca32a1fd05c6a0d8e91e93b9eab
Tsukyko <zoeamat.pro@gmail.com>
2022-02-17 16:33:08 +0100

    Tests: write sign in specs

--- cypress/support/constants.ts
+++ cypress/support/constants.ts
@@ -8,13 +8,19 @@ export enum URLS {
   'SIGN_OUT_PAGE' = '/sign-out',
   'SIGN_UP_PAGE' = '/sign-up',
   'ACCOUNT_PAGE' = '/account',
+  'FORGOT_PASSWORD_PAGE' = '/forgot-password',
 }
 //TODO: List missing pages
 
 export enum SELECTORS {
   'NAVBAR' = 'header',
+  'PAGE_TITLE' = 'h1',
   'BURGER_BUTTON' = 'button[data-cy="burgerButton"]',
   'NAV_MENU' = '[data-cy="navMenu"]',
+  'USERNAME_INPUT' = 'input[name="username"]',
+  'PASSWORD_INPUT' = 'input[name="password"]',
+  'REMEMBER_ME_INPUT' = 'input[name="rememberMe"]',
+  'SIGNIN_BUTTON' = 'button[name="signIn"]',
 }
 
 export enum DEVICES {
