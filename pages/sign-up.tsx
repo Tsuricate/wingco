cfa3ebf605462202423634f090a5a2fd772f6e98
Tsukyko <zoeamat.pro@gmail.com>
2022-02-17 18:21:01 +0100

    Feature: implement sign up page

---
+++ pages/sign-up.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  Text,
+} from '@chakra-ui/react';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
+import React from 'react';
+import FormLayout from '../components/FormLayout';
+import PageLayout from '../components/PageLayout';
+
+const SignUp: React.FC = () => {
+  const { t } = useTranslation(['signUp']);
+
+  return (
+    <PageLayout title={t('title', { ns: 'signUp' })}>
+      <Heading as="h1">{t('title', { ns: 'signUp' })}</Heading>
+      <FormLayout>
+        <FormControl>
+          <FormLabel htmlFor="username">{t('usernameLabel', { ns: 'signUp' })}</FormLabel>
+          <Input id="username" name="username" />
+          <FormHelperText>{t('usernameHelperText', { ns: 'signUp' })}</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="email">{t('emailLabel', { ns: 'signUp' })}</FormLabel>
+          <Input id="email" name="email" type="email" />
+          <FormHelperText>{t('emailHelperText', { ns: 'signUp' })}</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password">{t('passwordLabel', { ns: 'signUp' })}</FormLabel>
+          <Input id="password" name="password" type="password" />
+          <FormHelperText>{t('passwordHelperText', { ns: 'signUp' })}</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="passwordValidation">
+            {t('passwordLabelValidation', { ns: 'signUp' })}
+          </FormLabel>
+          <Input id="passwordValidation" name="passwordValidation" type="password" />
+          <FormHelperText>{t('passwordValidationHelperText', { ns: 'signUp' })}</FormHelperText>
+        </FormControl>
+        <Button type="submit" name="signUp">
+          {t('signUpButtonLabel', { ns: 'signUp' })}
+        </Button>
+        <Text>{t('alreadyRegistered', { ns: 'signUp' })}</Text>
+        <Link href="/sign-up">{t('', { ns: 'signUp' })}</Link>
+      </FormLayout>
+    </PageLayout>
+  );
+};
+
+export default SignUp;
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['signUp', 'common'])),
+  },
+});
