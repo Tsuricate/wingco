f6462e01f08ee3c4fcbe6821d5e087a80b1af042
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-02-19 22:00:19 +0100

    feature: Link can now be displayed as if it was a button

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import { Button as ChakraButton, ButtonProps, Link as ChakraLink } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { Link as ChakraLink } from '@chakra-ui/react';
+import React from 'react';
+import { buttonDefaultStyle } from './Button';
 
 interface LinkProps {
   href: string;
   dataCy?: string;
+  asButton?: boolean;
+  buttonVariant?: ButtonProps['variant'];
 }
 
-const Link: React.FC<LinkProps> = ({ href, children, dataCy }) => {
+const Link: React.FC<LinkProps> = ({
+  href,
+  children,
+  dataCy,
+  asButton = false,
+  buttonVariant = 'outline',
+}) => {
   return (
     <NextLink href={href} passHref>
-      <ChakraLink data-cy={dataCy}>{children}</ChakraLink>
+      {asButton ? (
+        <ChakraButton
+          as={ChakraLink}
+          data-cy={dataCy}
+          css={{
+            ':hover': {
+              textDecoration: 'none',
+            },
+          }}
+          variant={buttonVariant}
+          {...buttonDefaultStyle}
+        >
+          {children}
+        </ChakraButton>
+      ) : (
+        <ChakraLink data-cy={dataCy}>{children}</ChakraLink>
+      )}
     </NextLink>
   );
 };
