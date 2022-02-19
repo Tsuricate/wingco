d8acc710e26b6a5337e5335a2458d2a1a7449f0d
Tsukyko <zoeamat.pro@gmail.com>
2022-02-19 18:12:02 +0100

    Feature: conditionally render step 1 or 2 in password assistance page

--- pages/password-assistance.tsx
+++ pages/password-assistance.tsx
@@ -1,17 +1,27 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React from 'react';
+import React, { useState } from 'react';
 import FormLayout from '../components/FormLayout';
 import PageLayout from '../components/PageLayout';
 import PasswordAssistStep1 from '../components/PasswordAssistStep1';
+import PasswordAssistStep2 from '../components/PasswordAssistStep2';
+import PasswordAssistStep3 from '../components/PasswordAssistStep3';
 
 const PasswordAssistance = () => {
   const { t } = useTranslation(['passwordAssistance', 'common']);
 
+  const [hasProvidedEmail, setHasProvidedEmail] = useState(false);
+
+  const handleSubmit = () => {
+    setHasProvidedEmail(true);
+  };
+
   return (
     <PageLayout title={t('title')}>
       <FormLayout>
-        <PasswordAssistStep1 />
+        {!hasProvidedEmail && <PasswordAssistStep1 onSubmit={handleSubmit} />}
+        {hasProvidedEmail && <PasswordAssistStep2 />}
+        <PasswordAssistStep3 />
       </FormLayout>
     </PageLayout>
   );
