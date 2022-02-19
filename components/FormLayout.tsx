7081aa98489d90622b93f8cfcccbc30877d8a805
Tsukyko <zoeamat.pro@gmail.com>
2022-02-19 18:08:34 +0100

    Chore: prevent form submission in FormLayout

--- components/FormLayout.tsx
+++ components/FormLayout.tsx
@@ -2,8 +2,12 @@ import { Stack } from '@chakra-ui/react';
 import React from 'react';
 
 const FormLayout: React.FC = ({ children }) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Stack spacing={5}>{children}</Stack>
     </form>
   );
