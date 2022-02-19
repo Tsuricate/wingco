e787d633d98effb0696c7e52c43234cfc92f5bf2
Tsukyko <zoeamat.pro@gmail.com>
2022-02-19 14:54:37 +0100

    Refacto: use FormControl component for sign up page

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,8 +1,9 @@
-import { Button, FormControl, FormHelperText, FormLabel, Input, Text } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
 import React from 'react';
+import FormControl from '../components/FormControl';
 import FormLayout from '../components/FormLayout';
 import PageLayout from '../components/PageLayout';
 
@@ -12,28 +13,30 @@ const SignUp: React.FC = () => {
   return (
     <PageLayout title={t('title', { ns: 'signUp' })}>
       <FormLayout>
-        <FormControl>
-          <FormLabel htmlFor="username">{t('usernameLabel', { ns: 'signUp' })}</FormLabel>
-          <Input id="username" name="username" />
-          <FormHelperText>{t('usernameHelperText', { ns: 'signUp' })}</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="email">{t('emailLabel', { ns: 'signUp' })}</FormLabel>
-          <Input id="email" name="email" type="email" />
-          <FormHelperText>{t('emailHelperText', { ns: 'signUp' })}</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="password">{t('passwordLabel', { ns: 'signUp' })}</FormLabel>
-          <Input id="password" name="password" type="password" />
-          <FormHelperText>{t('passwordHelperText', { ns: 'signUp' })}</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="passwordValidation">
-            {t('passwordLabelValidation', { ns: 'signUp' })}
-          </FormLabel>
-          <Input id="passwordValidation" name="passwordValidation" type="password" />
-          <FormHelperText>{t('passwordValidationHelperText', { ns: 'signUp' })}</FormHelperText>
-        </FormControl>
+        <FormControl
+          id="username"
+          name="username"
+          label={t('usernameLabel', { ns: 'signUp' })}
+          helperText={t('usernameHelperText', { ns: 'signUp' })}
+        />
+        <FormControl
+          id="email"
+          name="email"
+          label={t('emailLabel', { ns: 'signUp' })}
+          helperText={t('emailHelperText', { ns: 'signUp' })}
+        />
+        <FormControl
+          id="password"
+          name="password"
+          label={t('passwordLabel', { ns: 'signUp' })}
+          helperText={t('passwordHelperText', { ns: 'signUp' })}
+        />
+        <FormControl
+          id="passwordValidation"
+          name="passwordValidation"
+          label={t('passwordLabelValidation', { ns: 'signUp' })}
+          helperText={t('passwordValidationHelperText', { ns: 'signUp' })}
+        />
         <Button type="submit" name="signUp">
           {t('signUpButtonLabel', { ns: 'signUp' })}
         </Button>
