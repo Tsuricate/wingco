53fa39c445b7c5d98fa4980c19b5b7a1368aff36
Tsukyko <zoeamat.pro@gmail.com>
2022-02-23 20:12:33 +0100

    Chore: create mock data for personal best scores

---
+++ mockData/bestScoreByCategory.ts
@@ -0,0 +1,15 @@
+const categories = [
+  'totalScore',
+  'birds',
+  'bonusCards',
+  'endOfRoundGoals',
+  'eggs',
+  'foodOnCards',
+  'tuckedCards',
+  'nectar',
+];
+
+export const bestScoreByCategory = categories.map((category) => ({
+  name: category,
+  score: Math.floor(Math.random() * 100),
+}));
