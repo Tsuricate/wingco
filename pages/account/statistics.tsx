6bac508064a4ee5e6d20d741b7024c5036f7aa05
Tsukyko <zoeamat.pro@gmail.com>
2022-02-23 20:13:15 +0100

    Feature: add best scores section in Statistics page + translations

--- pages/account/statistics.tsx
+++ pages/account/statistics.tsx
@@ -1,4 +1,4 @@
-import { Stat, StatLabel, StatNumber } from '@chakra-ui/react';
+import { List, ListItem, Stat, StatLabel, StatNumber, Text } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
@@ -6,9 +6,11 @@ import PageLayout from '../../components/layout/PageLayout';
 import StatisticsPanel from '../../components/StatisticsPanel';
 import VersusPanel from '../../components/VersusPanel';
 import { statisticsVersusData } from '../../mockData/statisticsVersusData';
+import { GiCrossedSabres, GiTrophy } from 'react-icons/gi';
+import { bestScoreByCategory } from '../../mockData/bestScoreByCategory';
 
 const Statistics: React.FC = () => {
-  const { t } = useTranslation('statistics');
+  const { t } = useTranslation(['statistics', 'common']);
 
   return (
     <PageLayout title={t('title')}>
@@ -16,8 +18,26 @@ const Statistics: React.FC = () => {
         <StatLabel> {t('victories')}</StatLabel>
         <StatNumber>45 / 156</StatNumber>
       </Stat>
-      <StatisticsPanel title={t('bestScores')} description={t('bestScoresDescription')} />
-      <StatisticsPanel title={t('versus')} description={t('versusDescription')}>
+      <StatisticsPanel
+        title={t('bestScores')}
+        description={t('bestScoresDescription')}
+        icon={GiTrophy}
+      >
+        <List>
+          {bestScoreByCategory.map((category) => (
+            <ListItem key={category.name}>
+              <Text>
+                {category.score} {t(`categories.${category.name}`, { ns: 'common' })}
+              </Text>
+            </ListItem>
+          ))}
+        </List>
+      </StatisticsPanel>
+      <StatisticsPanel
+        title={t('versus')}
+        description={t('versusDescription')}
+        icon={GiCrossedSabres}
+      >
         {statisticsVersusData.map((stat) => (
           <VersusPanel key={stat.title} {...stat} />
         ))}
