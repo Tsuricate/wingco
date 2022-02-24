5f56e2d667518eb75e23dc6d531d05bc4f74a4a4
Tsukyko <zoeamat.pro@gmail.com>
2022-02-24 12:06:37 +0100

    Feature: check url parameter in password assistance in order to access step 2 directly if user comes from manage account

--- pages/password-assistance.tsx
+++ pages/password-assistance.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import FormLayout from '../components/layout/FormLayout';
 import PageLayout from '../components/layout/PageLayout';
 import PasswordAssistStep1 from '../components/PasswordAssistStep1';
@@ -9,9 +10,16 @@ import PasswordAssistStep3 from '../components/PasswordAssistStep3';
 
 const PasswordAssistance = () => {
   const { t } = useTranslation(['passwordAssistance', 'common']);
+  const { query } = useRouter();
 
   const [hasProvidedEmail, setHasProvidedEmail] = useState(false);
   const [hasCorrectResetCode, setHasCorrectResetCode] = useState(false);
+
+  useEffect(() => {
+    if (query.email) {
+      setHasProvidedEmail(true);
+    }
+  }, [query.email]);
 
   const handleSubmitStep1 = () => {
     setHasProvidedEmail(true);
