d051bee50216ecf68177b257205195d885ccf221
Tsukyko <zoeamat.pro@gmail.com>
2022-02-25 14:46:49 +0100

    Feature: allow user to chose an avatar

--- components/AvatarSelector.tsx
+++ components/AvatarSelector.tsx
@@ -1,17 +1,25 @@
-import { Avatar, useDisclosure } from '@chakra-ui/react';
+import { Avatar, Image, SimpleGrid, useDisclosure } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
-import React from 'react';
+import React, { useState } from 'react';
+import { avatarImages } from '../mockData/avatarImages';
 import Modal from './Modal';
 
 const AvatarSelector: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { t } = useTranslation(['newGame', 'common']);
+  const [chosenAvatar, setChosenAvatar] = useState('');
+
   const handleClick = () => {
     onOpen();
   };
+
+  const handleChosenAvatar = (avatar: string) => {
+    setChosenAvatar(avatar);
+  };
+
   return (
     <>
-      <Avatar size="md" onClick={handleClick} />
+      <Avatar size="md" src={chosenAvatar} onClick={handleClick} />
       <Modal
         onClose={onClose}
         isOpen={isOpen}
@@ -19,7 +27,20 @@ const AvatarSelector: React.FC = () => {
         description={t('chooseAvatarDescription')}
         closeMessage={t('cancel', { ns: 'common' })}
         saveMessage={t('save', { ns: 'common' })}
-      ></Modal>
+      >
+        <SimpleGrid columns={{ base: 2 }} spacing={10}>
+          {avatarImages.map((image) => (
+            <Image
+              key={image.src}
+              boxSize="100px"
+              src={image.src}
+              alt={image.alt}
+              objectFit="cover"
+              onClick={() => handleChosenAvatar(image.src)}
+            />
+          ))}
+        </SimpleGrid>
+      </Modal>
     </>
   );
 };
