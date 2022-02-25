1a5b00e465beb7c4e71179cb14ae2a76164b38d8
Tsukyko <zoeamat.pro@gmail.com>
2022-02-25 16:39:43 +0100

    Refacto: dynamic avatar size for PlayerAvatar component

--- components/PlayerAvatar.tsx
+++ components/PlayerAvatar.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarBadge, Stack, StackDirection, Text } from '@chakra-ui/react';
+import { Avatar, AvatarBadge, Stack, StackDirection, Text, ThemingProps } from '@chakra-ui/react';
 import React from 'react';
 import { PLAYER_BADGE } from '../models/players';
 import { getBadgeColor } from '../utils/playersUtils';
@@ -7,14 +7,20 @@ interface PlayerAvatar {
   playerName: string;
   direction?: StackDirection;
   badge?: PLAYER_BADGE;
+  avatarSize?: ThemingProps<'Avatar'>['size'];
 }
 
-const PlayerAvatar: React.FC<PlayerAvatar> = ({ direction = 'row', playerName, badge }) => {
+const PlayerAvatar: React.FC<PlayerAvatar> = ({
+  direction = 'row',
+  playerName,
+  badge,
+  avatarSize = 'sm',
+}) => {
   const badgeColor = badge ? getBadgeColor(badge) : undefined;
 
   return (
     <Stack direction={direction} align="center">
-      <Avatar name={playerName} size="sm">
+      <Avatar name={playerName} size={avatarSize}>
         {badge && <AvatarBadge boxSize="1.25em" bg={badgeColor} />}
       </Avatar>
       <Text>{playerName}</Text>
