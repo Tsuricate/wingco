8c71ab3999aeacf4493bb4b3a58cf63d28cccee2
Tsukyko <zoeamat.pro@gmail.com>
2022-02-27 18:12:23 +0100

    Feature: implement Scores Sections in gameScores page

--- pages/game-scores.tsx
+++ pages/game-scores.tsx
@@ -1,14 +1,28 @@
+import { Stack } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
 import FormLayout from '../components/layout/FormLayout';
 import PageLayout from '../components/layout/PageLayout';
+import ScoresSection from '../components/ScoresSection';
+import { categories } from '../mockData/bestScoreByCategory';
+import players from '../mockData/fakePlayers';
 
 const GameScores: React.FC = () => {
-  const { t } = useTranslation('gameScores');
+  const { t } = useTranslation(['gameScores', 'common']);
   return (
     <PageLayout title={t('title')}>
-      <FormLayout></FormLayout>
+      <FormLayout>
+        <Stack spacing={5}>
+          {categories.map((category) => (
+            <ScoresSection
+              key={category}
+              title={t(`categories.${category}`, { ns: 'common' })}
+              players={players}
+            />
+          ))}
+        </Stack>
+      </FormLayout>
     </PageLayout>
   );
 };
