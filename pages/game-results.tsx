770bfd64c94bc7a648f3854d907161800809a5fa
Tsukyko <zoeamat.pro@gmail.com>
2022-03-01 15:35:51 +0100

    Feature: display New Record components in Result page + translations

--- pages/game-results.tsx
+++ pages/game-results.tsx
@@ -1,24 +1,35 @@
 import { Stack, Text } from '@chakra-ui/react';
+import uniqid from 'uniqid';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
 import PageLayout from '../components/layout/PageLayout';
+import NewRecord from '../components/NewRecord';
 import PlayerAvatar from '../components/PlayerAvatar';
-import { gameResults } from '../mockData/gameResults';
+import { gameResults, newRecords } from '../mockData/gameResults';
 
 const GameResults: React.FC = () => {
   const { t } = useTranslation('gameResults');
 
   return (
     <PageLayout title={t('gameResults:title')}>
-      <Stack>
+      <Stack spacing={5}>
         {gameResults.map((player) => (
           <Stack key={player.id} direction="row" align="center">
             <PlayerAvatar playerName={player.name} badge={player.badge} avatarSize="md" />
             <Text>
-              {player.score} {t('common:points')}
+              : {player.score} {t('common:points')}
             </Text>
           </Stack>
+        ))}
+        {newRecords.map((record) => (
+          <NewRecord
+            key={uniqid()}
+            newRecord={record.newRecord}
+            category={record.category}
+            playerName={record.playerName}
+            previousRecord={record.previousRecord}
+          />
         ))}
       </Stack>
     </PageLayout>
