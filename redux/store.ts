1faac96017b765e7e7ba6a13fd8ba996ec898c69
Tsukyko <zoeamat.pro@gmail.com>
2022-03-08 16:41:12 +0100

    Config: add redux dev tools in store

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, Middleware } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
+import authMiddleware from './middlewares/signUpMiddleware';
 
-const makeStore = () => createStore(rootReducer);
+const middlewares = [authMiddleware];
+
+const bindMiddlewares = (middlewares: Array<Middleware>) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return composeWithDevTools(applyMiddleware(...middlewares));
+  }
+  return applyMiddleware(...middlewares);
+};
+
+const makeStore = () => createStore(rootReducer, bindMiddlewares(middlewares));
 
 export const wrapper = createWrapper(makeStore, { debug: true });
