399ace2f245c4d780670478bf90666e011b56bcd
Tsukyko <zoeamat.pro@gmail.com>
2022-03-08 14:39:58 +0100

    Update: auth actions types & creator

--- redux/actions/auth.ts
+++ redux/actions/auth.ts
@@ -1,6 +1,17 @@
 export const UPDATE_EMAIL = 'UPDATE_EMAIL';
+export const SUBMIT_SIGN_UP = 'SUBMIT_SIGN_UP';
+export const SAVE_USER = 'SAVE_USER';
 
 export const updateEmail = (email: string) => ({
   type: UPDATE_EMAIL,
   email,
+});
+
+export const submitSignUp = () => ({
+  type: SUBMIT_SIGN_UP,
+});
+
+export const saveUser = (isLogged: boolean) => ({
+  type: SAVE_USER,
+  isLogged,
 });
