4d449f16bc6bd98598517726b084bd6cf1505871
Tsukyko <zoeamat.pro@gmail.com>
2022-03-08 17:10:49 +0100

    Chore: change name from auth to signUp in store and combine reducers

--- redux/store.ts
+++ redux/store.ts
@@ -2,9 +2,9 @@ import { applyMiddleware, createStore, Middleware } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import authMiddleware from './middlewares/signUpMiddleware';
+import signUpMiddleware from './middlewares/signUpMiddleware';
 
-const middlewares = [authMiddleware];
+const middlewares = [signUpMiddleware];
 
 const bindMiddlewares = (middlewares: Array<Middleware>) => {
   if (process.env.NODE_ENV !== 'production') {
