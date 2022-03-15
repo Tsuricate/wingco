88300e49a102ae9772d6e58b6e07f444c9b901e2
Tsukyko <zoeamat.pro@gmail.com>
2022-03-15 16:28:05 +0100

    Feature: create new api endpoint for email validation token

---
+++ pages/api/validate-email/[validationToken].tsx
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '../../../apollo-client';
+import { VERIFY_VALIDATION_EMAIL_TOKEN } from '../../../queries/signup.queries';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { validationToken } = req.query;
+
+  client
+    .query({
+      query: VERIFY_VALIDATION_EMAIL_TOKEN,
+      variables: { validationEmailToken: validationToken },
+    })
+    .then((response) => {
+      if (response.data.player) {
+        res.status(200).redirect('/sign-in');
+      } else {
+        res.status(400).json('Something went wrong !');
+      }
+    })
+    .catch((err) => {
+      console.log('Error ?  : ', err);
+    });
+};
+
+export default handler;
