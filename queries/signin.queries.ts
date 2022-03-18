521a5eb688dc0ffcc2282b6aa5356614c08d9a9e
Tsukyko <zoeamat.pro@gmail.com>
2022-03-18 16:49:53 +0100

    Feature: create new query to find player with a validated email address and obtain his password

---
+++ queries/signin.queries.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const FIND_VERIFIED_PLAYER = gql`
+  query findVerifiedPlayer($email: String!) {
+    players(where: { email: $email, hasVerifiedEmail: true }) {
+      id
+      name
+      password
+    }
+  }
+`;
