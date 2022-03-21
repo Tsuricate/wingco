9db8262c9000372f848149e7bd583612355922a3
Tsukyko <zoeamat.pro@gmail.com>
2022-03-21 14:28:25 +0100

    Feature: avatar and player name now displayed in account page when sign in

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -2,14 +2,17 @@ import { Center, Stack } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AccountPanel from '../../components/AccountPanel';
 import Button from '../../components/Button';
 import PageLayout from '../../components/layout/PageLayout';
 import PlayerAvatar from '../../components/PlayerAvatar';
 import { panels } from '../../data/account';
+import { RootState } from '../../redux/reducers';
 
 const Account: React.FC = () => {
   const { t } = useTranslation(['account', 'common']);
+  const { name, avatar } = useSelector((state: RootState) => state.auth);
 
   const handleSignOut = () => {
     console.log('Sign out button was clicked!');
@@ -18,7 +21,7 @@ const Account: React.FC = () => {
     <PageLayout title={t('account:title')}>
       <Stack spacing={8}>
         <Stack as={Center}>
-          <PlayerAvatar playerName="Lorem Ipsum" direction="column" avatarSize="lg" />
+          <PlayerAvatar playerName={name} avatar={avatar?.url} direction="column" avatarSize="lg" />
           <Button dataCy="signOut" onClick={handleSignOut}>
             {t('common:signOut')}
           </Button>
