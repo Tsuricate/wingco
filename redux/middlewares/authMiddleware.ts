5f3e7aaad823957f1807f6e3bd6a9d8d025dc90f
Tsukyko <zoeamat.pro@gmail.com>
2022-03-27 16:58:13 +0200

    Feature: create authMiddleware in order to handle connexion

---
+++ redux/middlewares/authMiddleware.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Action, Dispatch, Middleware } from 'redux';
+import { saveUser } from '../actions/auth';
+import { SUBMIT_SIGN_IN, updateErrorSignIn } from '../actions/signIn';
+
+const authMiddleware: Middleware = (store) => (next: Dispatch) => (action: Action) => {
+  switch (action.type) {
+    case SUBMIT_SIGN_IN: {
+      const { email, password, rememberMe } = store.getState().signIn;
+
+      axios
+        .post('/api/sign-in', { email, password })
+        .then((res) => {
+          store.dispatch(updateErrorSignIn(false));
+          store.dispatch(saveUser(res.data.player, rememberMe));
+        })
+        .catch(() => {
+          store.dispatch(updateErrorSignIn(true));
+        });
+
+      next(action);
+      break;
+    }
+    default:
+      next(action);
+  }
+};
+
+export default authMiddleware;
--- redux/store.ts
+++ redux/store.ts
@@ -3,8 +3,9 @@ import { createWrapper } from 'next-redux-wrapper';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import signUpMiddleware from './middlewares/signUpMiddleware';
+import authMiddleware from './middlewares/authMiddleware';
 
-const middlewares = [signUpMiddleware];
+const middlewares = [signUpMiddleware, authMiddleware];
 
 const bindMiddlewares = (middlewares: Array<Middleware>) => {
   if (process.env.NODE_ENV !== 'production') {
