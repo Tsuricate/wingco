b9fc68b287ecb59b73386b5f94f7155f757ca39e
Tsukyko <zoeamat.pro@gmail.com>
2022-03-27 16:54:40 +0200

    Feature: handle errors email & password when sign in

--- redux/actions/signIn.ts
+++ redux/actions/signIn.ts
@@ -1,5 +1,16 @@
+export const SUBMIT_SIGN_IN = 'SUBMIT_SIGN_IN';
+export const UPDATE_ERROR_SIGN_IN = 'UPDATE_ERROR_SIGN_IN';
 export const UPDATE_SIGN_IN_INFOS = 'UPDATE_SIGN_IN_INFOS';
 export const UPDATE_REMEMBER_ME = 'UPDATE_REMEMBER_ME';
+
+export const submitSignIn = () => ({
+  type: SUBMIT_SIGN_IN,
+});
+
+export const updateErrorSignIn = (error: boolean) => ({
+  type: UPDATE_ERROR_SIGN_IN,
+  error,
+});
 
 export const updateSignInInfos = (value: string, name: string) => ({
   type: UPDATE_SIGN_IN_INFOS,
