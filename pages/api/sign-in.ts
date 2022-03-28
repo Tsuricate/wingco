90782ce2d1dea7d9ddde3c627e2ad9237c1c5a07
Tsukyko <zoeamat.pro@gmail.com>
2022-03-28 19:25:12 +0200

    Feature: create token and send it in http only cookie

--- pages/api/sign-in.ts
+++ pages/api/sign-in.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 import { findVerifiedPlayerByEmail } from '../../utils/api/playerUtils';
 import { comparePassword } from '../../utils/password';
 
@@ -11,7 +13,26 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (player) {
       const isPasswordValid = comparePassword(password, player.password);
       if (isPasswordValid) {
-        res.status(200).json({ player });
+        const payload = {
+          id: player.id,
+        };
+        const jwtSecret: jwt.Secret = process.env.JWT_SECRET_KEY || '';
+        const token = jwt.sign(payload, jwtSecret, {
+          expiresIn: 60 * 60 * 24 * 7,
+        });
+
+        res.setHeader(
+          'Set-Cookie',
+          cookie.serialize('authToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            maxAge: 60 * 60 * 24 * 7,
+            sameSite: 'strict',
+            path: '/',
+          })
+        );
+
+        res.status(200).json({ success: true, player });
       }
     }
   } catch (err) {
