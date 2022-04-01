a3b02c8116ab316b939eb9d02c85b04330e39e52
Tsukyko <zoeamat.pro@gmail.com>
2022-04-01 19:23:08 +0200

    Feature: display avatar instead of burger icon for navbar menu when player is logged

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,33 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { Box, Flex, Heading, IconButton, Stack, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { PLAYER_BADGE } from '../models/players';
+import { RootState } from '../redux/reducers';
 import ColorModeToggle from './ColorModeToggle';
 import NavMenu from './NavMenu';
+import PlayerAvatar from './PlayerAvatar';
 
 const NavBar: React.FC = () => {
+  const { isLogged, avatar } = useSelector((state: RootState) => state.auth);
   const { isOpen, onClose, onToggle } = useDisclosure();
 
   return (
     <Box as="header" p={2}>
       <Flex align="center" justify="space-between">
         <Heading>WingCo</Heading>
-        <Stack direction="row" display={{ md: 'none' }}>
+        <Stack direction="row" display={{ base: 'flex', md: 'none' }}>
           <ColorModeToggle />
-          <IconButton
-            data-cy="burgerButton"
-            aria-label="Open menu"
-            icon={<HamburgerIcon />}
-            onClick={onToggle}
-          />
+          {isLogged ? (
+            <PlayerAvatar badge={PLAYER_BADGE.Logged} avatar={avatar} onClick={onToggle} />
+          ) : (
+            <IconButton
+              data-cy="burgerButton"
+              aria-label="Open menu"
+              icon={<HamburgerIcon />}
+              onClick={onToggle}
+            />
+          )}
         </Stack>
         <Box display={{ base: 'none', md: 'block' }}>
           <NavMenu direction="row" />
