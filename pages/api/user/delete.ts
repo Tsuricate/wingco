a457e7bec51562359d380efb214e1b9d2cd5978e
Tsukyko <zoeamat.pro@gmail.com>
2022-04-13 18:28:05 +0200

    Feature: create new api endpoint to delete player (if valid token) then redirect to home page

---
+++ pages/api/user/delete.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import cookie from 'cookie';
+import jwt from 'jsonwebtoken';
+import { DELETE_PLAYER_BY_ID } from '../../../queries/auth.queries';
+import client from '../../../apollo-client';
+import { deleteAuthToken } from '../../../utils/api/auth';
+
+interface JWTContent {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.headers.cookie) {
+    res.status(404).json({ message: 'Unauthenticated user' });
+  } else {
+    try {
+      const { authToken } = cookie.parse(req.headers.cookie);
+      const jwtSecret: jwt.Secret = process.env.JWT_SECRET_KEY || '';
+
+      const payload = jwt.verify(authToken, jwtSecret) as JWTContent;
+
+      if (payload.id) {
+        const player = await client.mutate({
+          mutation: DELETE_PLAYER_BY_ID,
+          variables: { id: payload.id },
+        });
+
+        if (player) {
+          deleteAuthToken(res);
+          res.redirect('/');
+        }
+      }
+    } catch (error) {
+      res.status(403).json(error);
+    }
+  }
+};
+
+export default handler;
