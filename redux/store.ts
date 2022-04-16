80eb619be28d18c8764b13d4e19822ceea2c94f8
Tsukyko <zoeamat.pro@gmail.com>
2022-04-16 20:19:11 +0200

    Fix: add passwordAssistanceMiddleware to store

--- redux/store.ts
+++ redux/store.ts
@@ -4,8 +4,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import signUpMiddleware from './middlewares/signUpMiddleware';
 import authMiddleware from './middlewares/authMiddleware';
+import passwordAssistanceMiddleware from './middlewares/passwordAssistanceMiddleware';
 
-const middlewares = [signUpMiddleware, authMiddleware];
+const middlewares = [signUpMiddleware, authMiddleware, passwordAssistanceMiddleware];
 
 const bindMiddlewares = (middlewares: Array<Middleware>) => {
   if (process.env.NODE_ENV !== 'production') {
