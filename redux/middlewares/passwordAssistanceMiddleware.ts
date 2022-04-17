0a183399cf52867e5a9d6a54a6ae96cb0444bb71
Tsukyko <zoeamat.pro@gmail.com>
2022-04-17 17:51:21 +0200

    Feature: handle verify reset code action in middleware

--- redux/middlewares/passwordAssistanceMiddleware.ts
+++ redux/middlewares/passwordAssistanceMiddleware.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { Action, Dispatch, Middleware } from 'redux';
-import { SEND_RESET_PASSWORD_EMAIL } from '../actions/passwordAssistance';
+import {
+  SEND_RESET_PASSWORD_EMAIL,
+  updateHasCorrectResetCode,
+  VERIFY_PASSWORD_RESET_CODE,
+} from '../actions/passwordAssistance';
 
 const passwordAssistanceMiddleware: Middleware =
   (store) => (next: Dispatch) => (action: Action) => {
@@ -9,6 +13,17 @@ const passwordAssistanceMiddleware: Middleware =
         {
           const { email } = store.getState().passwordAssistance;
           axios.post('/api/send-email', { email });
+        }
+        next(action);
+        break;
+
+      case VERIFY_PASSWORD_RESET_CODE:
+        {
+          const { resetCode } = store.getState().passwordAssistance;
+          axios
+            .post('/api/user/verify-reset-code', { resetCode })
+            .then(() => store.dispatch(updateHasCorrectResetCode(true)))
+            .catch(() => store.dispatch(updateHasCorrectResetCode(false)));
         }
         next(action);
         break;
