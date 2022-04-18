aba2d8170ecbff8a97f8b98007c7c3675963cf0c
Tsukyko <zoeamat.pro@gmail.com>
2022-04-18 16:16:51 +0200

    Fix: return promise for send-email api

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -5,25 +5,29 @@ import { getResetPasswordMessage, getSignUpMessage } from '../../utils/api/getEm
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { userId, username, email } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: 'smtp.gmail.com',
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
+  return new Promise(async (resolve, reject) => {
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
 
-  const message = userId
-    ? await getSignUpMessage(userId, email, username)
-    : await getResetPasswordMessage(email);
+    const message = userId
+      ? await getSignUpMessage(userId, email, username)
+      : await getResetPasswordMessage(email);
 
-  transporter.sendMail(message, (err, info) => {
-    if (err) {
-      res.status(400).json(err);
-    } else {
-      res.status(200).json(info);
-    }
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        res.status(400).json(err);
+        reject();
+      } else {
+        res.status(200).json(info);
+        resolve();
+      }
+    });
   });
 };
 
