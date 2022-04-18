f145e6533463e4f286b60eea3c4a885a607e90e4
Tsukyko <zoeamat.pro@gmail.com>
2022-04-18 16:19:25 +0200

    Feature: add action types & creators for submit reset code and loading state

--- redux/actions/passwordAssistance.ts
+++ redux/actions/passwordAssistance.ts
@@ -1,7 +1,9 @@
 export const UPDATE_PASSWORD_ASSISTANCE_INFOS = 'UPDATE_PASSWORD_ASSISTANCE_INFOS';
 export const SEND_RESET_PASSWORD_EMAIL = 'SEND_RESET_PASSWORD_EMAIL';
 export const VERIFY_PASSWORD_RESET_CODE = 'VERIFY_PASSWORD_RESET_CODE';
+export const UPDATE_HAS_SUBMIT_RESET_CODE = 'UPDATE_HAS_SUBMIT_RESET_CODE';
 export const UPDATE_HAS_CORRECT_RESET_CODE = 'UPDATE_HAS_CORRECT_RESET_CODE';
+export const UPDATE_IS_LOADING = 'UPDATE_IS_LOADING';
 
 export const updatePasswordAssistanceInfos = (value: string, name: string) => ({
   type: UPDATE_PASSWORD_ASSISTANCE_INFOS,
@@ -17,7 +19,17 @@ export const verifyPasswordResetCode = () => ({
   type: VERIFY_PASSWORD_RESET_CODE,
 });
 
+export const updateHasSubmitResetCode = (value: boolean) => ({
+  type: UPDATE_HAS_SUBMIT_RESET_CODE,
+  value,
+});
+
 export const updateHasCorrectResetCode = (value: boolean) => ({
   type: UPDATE_HAS_CORRECT_RESET_CODE,
+  value,
+});
+
+export const updateIsLoading = (value: boolean) => ({
+  type: UPDATE_IS_LOADING,
   value,
 });
