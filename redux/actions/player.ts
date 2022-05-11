25f532ece97f2d21d493441be8d746e8c517227b
Tsukyko <zoeamat.pro@gmail.com>
2022-05-11 11:44:02 +0200

    Feature: create action types&creators to update and save new player avatar

--- redux/actions/player.ts
+++ redux/actions/player.ts
@@ -2,6 +2,7 @@ import { AvatarImage } from '../../models/players';
 
 export const GET_AVATAR_IMAGES = 'GET_AVATAR_IMAGES';
 export const SAVE_AVATAR_IMAGES = 'SAVE_AVATAR_IMAGES';
+export const UPDATE_PLAYER_AVATAR = 'UDPATE_PLAYER_AVATAR';
 
 export const getAvatarImages = () => ({
   type: GET_AVATAR_IMAGES,
@@ -10,4 +11,9 @@ export const getAvatarImages = () => ({
 export const saveAvatarImages = (avatarImages: Array<AvatarImage>) => ({
   type: SAVE_AVATAR_IMAGES,
   avatarImages,
+});
+
+export const updatePlayerAvatar = (newAvatarId: string) => ({
+  type: UPDATE_PLAYER_AVATAR,
+  newAvatarId,
 });
