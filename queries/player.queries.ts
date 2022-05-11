b9775449b7b3a16ddf37ed2e4dc1c8528c37f8d2
Tsukyko <zoeamat.pro@gmail.com>
2022-05-11 11:46:40 +0200

    Feature: update getAvatar query + create change avatar query to update player in DB

--- queries/player.queries.ts
+++ queries/player.queries.ts
@@ -3,7 +3,18 @@ import { gql } from '@apollo/client';
 export const GET_AVATAR_IMAGES = gql`
   query getAvatarImages {
     assets {
+      id
       url(transformation: { image: { resize: { height: 96, width: 96, fit: crop } } })
+    }
+  }
+`;
+
+export const CHANGE_PLAYER_AVATAR = gql`
+  mutation UpdatePlayerAvatar($playerId: ID!, $avatarId: ID!) {
+    updatePlayer(data: { avatar: { connect: { id: $avatarId } } }, where: { id: $playerId }) {
+      avatar {
+        url
+      }
     }
   }
 `;
