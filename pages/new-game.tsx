2f9e988cd0bab606cdf0794645369db94cc396be
Tsukyko <zoeamat.pro@gmail.com>
2022-05-12 15:09:51 +0200

    Feature: get player connected as first player when creating a game

--- pages/new-game.tsx
+++ pages/new-game.tsx
@@ -1,28 +1,41 @@
-import uniqid from 'uniqid';
 import { FormControl, FormLabel, Stack, Switch, Text } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../components/Button';
-import InvitePlayerButton from '../components/InvitePlayerButton';
 import Form from '../components/Form';
+import InvitePlayerButton from '../components/InvitePlayerButton';
 import PageLayout from '../components/layout/PageLayout';
 import NewGamePlayer from '../components/NewGamePlayer';
+import { saveFirstPlayer } from '../redux/actions/newGame';
+import { RootState } from '../redux/reducers';
 
 const NewGame: React.FC = () => {
   const { t } = useTranslation('newGame');
-  const [players, setPlayers] = useState([{ id: uniqid() }]);
+  const dispatch = useDispatch();
+  const { isLogged, id, name, avatar } = useSelector((state: RootState) => state.auth);
+  const { players } = useSelector((state: RootState) => state.game);
   const gameId = '#156D5E8';
 
-  const handleAddPlayer = () => {
-    if (players.length < 5) {
-      setPlayers([...players, { id: uniqid() }]);
+  useEffect(() => {
+    if (isLogged && players.length < 1) {
+      dispatch(saveFirstPlayer({ id, name, avatar }));
     }
+  }, [avatar, dispatch, id, isLogged, name, players.length]);
+
+  const handleAddPlayer = () => {
+    console.log('Add player');
+    // if (players.length < 5) {
+    //   setPlayers([...players, { id: uniqid() }]);
+    // }
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleDeletePlayer = (playerId: string) => {
-    const newPlayersList = players.filter((player) => player.id !== playerId);
-    setPlayers([...newPlayersList]);
+    console.log('Remove player');
+    // const newPlayersList = players.filter((player) => player.id !== playerId);
+    // setPlayers([...newPlayersList]);
   };
 
   const handleSubmit = () => {
@@ -37,6 +50,9 @@ const NewGame: React.FC = () => {
           {players.map((player, index) => (
             <NewGamePlayer
               key={player.id}
+              id={player.id}
+              name={player.name}
+              avatar={player.avatar}
               playerNumber={index + 1}
               onDeletePlayer={() => handleDeletePlayer(player.id)}
             />
