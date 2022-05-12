d134bd406f5cdd6bef0cd8fbfd2c19b965b7ca74
Tsukyko <zoeamat.pro@gmail.com>
2022-05-12 15:12:17 +0200

    Feature: create action type/creator to save first player

---
+++ redux/actions/newGame.ts
@@ -0,0 +1,7 @@
+export const SAVE_FIRST_PLAYER = 'SAVE_FIRST_PLAYER';
+import NewGamePlayer from '../../models/players';
+
+export const saveFirstPlayer = (newPlayer: NewGamePlayer) => ({
+  type: SAVE_FIRST_PLAYER,
+  newPlayer,
+});
