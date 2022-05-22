6a81eec0604d027143c2f5ca5113fa1e2acb8061
Tsukyko <zoeamat.pro@gmail.com>
2022-05-22 15:50:57 +0200

    Chore: create action creators/types for creating and save a new game

--- redux/actions/newGame.ts
+++ redux/actions/newGame.ts
@@ -5,6 +5,8 @@ export const REMOVE_PLAYER = 'REMOVE_PLAYER';
 export const UPDATE_PLAYER_INFOS = 'UPDATE_PLAYER_INFOS';
 export const RESET_GAME_INFOS = 'RESET_GAME_INFOS';
 export const UPDATE_GAME_WITH_NECTAR = 'UPDATE_GAME_WITH_NECTAR';
+export const CREATE_NEW_GAME = 'CREATE_NEW_GAME';
+export const SAVE_NEW_GAME = 'SAVE_NEW_GAME';
 
 export const addPlayer = (newPlayer: InewGamePlayer) => ({
   type: ADD_PLAYER,
@@ -29,4 +31,14 @@ export const resetGameInfos = () => ({
 export const updateGameWithNectar = (value: boolean) => ({
   type: UPDATE_GAME_WITH_NECTAR,
   value,
+});
+
+export const createNewGame = () => ({
+  type: CREATE_NEW_GAME,
+});
+
+export const saveNewGame = (id: string, slug: string) => ({
+  type: SAVE_NEW_GAME,
+  id,
+  slug,
 });
