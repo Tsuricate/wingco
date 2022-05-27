60a34c65c094a88c8ff6a54fff2941b2f51bd9e1
Tsukyko <zoeamat.pro@gmail.com>
2022-05-27 16:42:04 +0200

    Feature: update create game api with new mutation

--- pages/api/game/create-game.ts
+++ pages/api/game/create-game.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../../apollo-client';
-import { CREATE_GAME } from '../../../queries/game.queries';
+import { CREATE_GAME_WITH_PLAYERS } from '../../../queries/game.queries';
+import { getParticipantsFromPlayers } from '../../../utils/game';
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Generate a slug of 6 random characters (letters and numbers)
-  const gameSlug = Math.random().toString(36).substring(2, 8).toUpperCase();
+  const { players, gameWithNectar, gameSlug, hostId } = req.body;
+
+  const participants = getParticipantsFromPlayers(players);
+
   try {
     const newGame = await client.mutate({
-      mutation: CREATE_GAME,
-      variables: { slug: gameSlug },
+      mutation: CREATE_GAME_WITH_PLAYERS,
+      variables: { participants, gameSlug, hostId, withOceaniaExpansion: gameWithNectar },
     });
 
-    if (newGame.data.createGame) {
-      res.status(201).json(newGame.data.createGame);
-    }
+    console.log('NewGame : ', newGame);
   } catch (err) {
     res.status(400).end();
   }
