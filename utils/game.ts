551c877c0adeff0893dc91fe6d7d77ec4e336c96
Tsukyko <zoeamat.pro@gmail.com>
2022-05-27 16:37:14 +0200

    Feature: create util function to get array of players either registered or unregistered

--- utils/game.ts
+++ utils/game.ts
@@ -1,3 +1,5 @@
+import { InewGamePlayer } from '../models/players';
+
 export const getEstimatedTime = (totalMinutes: number) => {
   const hours = totalMinutes / 60;
   const rhours = Math.floor(hours);
@@ -6,4 +8,26 @@ export const getEstimatedTime = (totalMinutes: number) => {
   const hDisplay = rhours > 0 ? rhours + ' h ' : '';
   const mDisplay = rminutes > 0 ? rminutes + (rminutes == 1 ? ' minute ' : ' minutes ') : '';
   return hDisplay + mDisplay;
+};
+
+export const getParticipantsFromPlayers = (players: Array<InewGamePlayer>) => {
+  return players.map((player) => {
+    return player.isRegistered
+      ? {
+          player: {
+            connect: {
+              id: player.id,
+            },
+          },
+        }
+      : {
+          player: {
+            create: {
+              name: player.name,
+              hasVerifiedEmail: false,
+              isRegistered: false,
+            },
+          },
+        };
+  });
 };
