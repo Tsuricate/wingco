c3e2d2481c56bd1e92dcb7696a5981cbd7cab624
Tsukyko <zoeamat.pro@gmail.com>
2022-06-12 18:04:30 +0200

    Feature: create new utils functions to get gameResults and all games ids

--- utils/game.ts
+++ utils/game.ts
@@ -7,7 +7,7 @@ import {
   PLAYER_BADGE,
   Score,
 } from '../models/players';
-import { GET_CATEGORIES } from '../queries/game.queries';
+import { GET_ALL_GAME_IDS, GET_CATEGORIES, GET_GAME_RESULTS } from '../queries/game.queries';
 
 export const getEstimatedTime = (totalMinutes: number) => {
   const hours = totalMinutes / 60;
@@ -120,6 +120,39 @@ export const getCategories = async () => {
     return categories;
   } catch (err) {
     throw new Error('Cannot fetch categories');
+  }
+};
+
+export const getGameResults = async (gameId: string) => {
+  try {
+    const {
+      data: {
+        game: { results },
+      },
+    } = await client.query({
+      query: GET_GAME_RESULTS,
+      variables: { gameId },
+    });
+    return results;
+  } catch (err) {
+    throw new Error('Cannot fetch game results');
+  }
+};
+
+export const getAllGameIds = async () => {
+  try {
+    const {
+      data: { games },
+    } = await client.query({
+      query: GET_ALL_GAME_IDS,
+    });
+    return games.map((game: { id: string }) => ({
+      params: {
+        gameId: game.id,
+      },
+    }));
+  } catch (err) {
+    throw new Error('Cannot fetch all games ids');
   }
 };
 
