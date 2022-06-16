dbe9614fbcfb1a05634d81757f0c422de02eb2a0
Tsukyko <zoeamat.pro@gmail.com>
2022-06-16 18:28:37 +0200

    Chore: rename interface

--- utils/statistics.ts
+++ utils/statistics.ts
@@ -1,6 +1,6 @@
-import { Rank, Statistics } from '../models/statistics';
+import { Rank, Victories } from '../models/statistics';
 
-export const getPlayerVictories = (playerStatistics: Array<Rank>): Statistics => {
+export const getPlayerVictories = (playerStatistics: Array<Rank>): Victories => {
   const victories = playerStatistics.filter((stat) => stat.rank === 1).length;
   const allGames = playerStatistics.length;
   return { victories, allGames };
