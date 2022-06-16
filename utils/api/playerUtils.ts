835f0299a37e73c4836100b0ba4e1e29aab99d6d
Tsukyko <zoeamat.pro@gmail.com>
2022-06-16 18:30:56 +0200

    Feature: new util function to get best scores by category for a player

--- utils/api/playerUtils.ts
+++ utils/api/playerUtils.ts
@@ -1,8 +1,11 @@
 import client from '../../apollo-client';
+import { ResultScore } from '../../models/game';
+import { BestPersonalScores } from '../../models/statistics';
 import { FIND_PLAYER_BY_ID } from '../../queries/auth.queries';
 import { GET_AVATAR_IMAGES, GET_PLAYER_STATISTICS } from '../../queries/player.queries';
 import { FIND_VERIFIED_PLAYER } from '../../queries/signin.queries';
 import { FIND_PLAYER } from '../../queries/signup.queries';
+import { getCategoriesFromScores } from '../gameResults';
 
 export const findPlayerByEmail = async (email: string) => {
   return await client.query({
@@ -65,4 +68,23 @@ export const getPlayerStatistics = async (playerId: string) => {
   } catch (err) {
     throw new Error('Cannont fetch player statistics');
   }
+};
+
+export const getBestScoresByCategory = (gameScores: Array<ResultScore>): Array<BestPersonalScores> => {
+  const categories = getCategoriesFromScores(gameScores);
+
+  const bestScoreByCategory = categories.map((category) => {
+    const previousScoresInCurrentCategory = gameScores
+      .filter((score) => score.category.name === category)
+      .map((score) => score.value);
+
+    const bestScore = Math.max(...previousScoresInCurrentCategory);
+
+    return {
+      category,
+      value: bestScore,
+    };
+  });
+
+  return bestScoreByCategory;
 };
