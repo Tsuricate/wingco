3463e28167a988bf0879ef1a7a72698c4a07fb62
Tsukyko <zoeamat.pro@gmail.com>
2022-06-20 15:11:45 +0200

    Fix: differents interfaces for joining game when user is logged

--- pages/join-game.tsx
+++ pages/join-game.tsx
@@ -2,14 +2,17 @@ import { Avatar, Stack, Text } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Button from '../components/Button';
 import Form from '../components/Form';
 import FormControl from '../components/FormControl';
 import PageLayout from '../components/layout/PageLayout';
 import Link from '../components/Link';
+import { RootState } from '../redux/reducers';
 
 const JoinGame: React.FC = () => {
   const { t } = useTranslation(['joinGame', 'common']);
+  const { isLogged } = useSelector((state: RootState) => state.auth);
 
   const updateField = () => {
     console.log('Update');
@@ -23,19 +26,23 @@ const JoinGame: React.FC = () => {
     <PageLayout title={t('joinGame:title')}>
       <Form onSubmit={handleSubmit}>
         <Text>{t('joinGame:description')}</Text>
-        <Link href="/sign-in" asButton>
-          {t('common:signIn')}
-        </Link>
-        <Stack direction="row" align="center">
-          <Avatar name="Lorem Ipsum" data-cy="avatarSelector" />
-          <FormControl
-            id="username"
-            name="username"
-            label={t('common:usernameLabel')}
-            helperText={t('common:usernameHelperText')}
-            updateField={updateField}
-          />
-        </Stack>
+        {!isLogged && (
+          <>
+            <Link href="/sign-in" asButton>
+              {t('common:signIn')}
+            </Link>
+            <Stack direction="row" align="center">
+              <Avatar name="Lorem Ipsum" data-cy="avatarSelector" />
+              <FormControl
+                id="username"
+                name="username"
+                label={t('common:usernameLabel')}
+                helperText={t('common:usernameHelperText')}
+                updateField={updateField}
+              />
+            </Stack>
+          </>
+        )}
         <FormControl
           id="gameId"
           name="gameId"
