afa292d3bc088f8006ee468d77674828c161bc26
Tsukyko <zoeamat.pro@gmail.com>
2022-06-21 16:51:36 +0200

    Feature: new api endpoint to handle join game requests with pusher

---
+++ pages/api/pusher/join-game.ts
@@ -0,0 +1,31 @@
+import { NextApiHandler } from 'next';
+import pusher from '../../../pusher-client';
+import { getUserInfosFromCookie } from '../../../utils/api/auth';
+import { findPlayerById } from '../../../utils/api/playerUtils';
+
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    const { gameSlug, player } = req.body;
+
+    if (!player) {
+      const { id } = getUserInfosFromCookie(req.headers.cookie);
+      const { name, avatar } = await findPlayerById(id);
+      const registeredPlayer = {
+        id,
+        name,
+        avatar,
+      };
+
+      await pusher.trigger(`game-${gameSlug}`, 'join-game-request', {
+        registeredPlayer,
+      });
+
+      res.json({ message: 'Request sent !' });
+    }
+  } catch (err) {
+    if (process.env.NODE_ENV !== 'production') console.log('Error : ', err);
+    res.status(400).end();
+  }
+};
+
+export default handler;
