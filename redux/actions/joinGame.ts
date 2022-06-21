36ec9c454910cc6da2ebaf1d36f9485c50a13fff
Tsukyko <zoeamat.pro@gmail.com>
2022-06-21 16:49:31 +0200

    Chore: create action type & creator for requesting to join a game

--- redux/actions/joinGame.ts
+++ redux/actions/joinGame.ts
@@ -1,7 +1,12 @@
 export const UPDATE_JOIN_GAME_SLUG = 'UPDATE_JOIN_GAME_SLUG';
+export const JOIN_GAME_REQUEST = 'JOIN_GAME_REQUEST';
 
 export const updateJoinGameSlug = (value: string, name: string) => ({
   type: UPDATE_JOIN_GAME_SLUG,
   value,
   name,
+});
+
+export const joinGameRequest = () => ({
+  type: JOIN_GAME_REQUEST,
 });
