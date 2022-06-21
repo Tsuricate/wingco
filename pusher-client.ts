790a2b19616fb040ab9879f05afdbd98ade1f13a
Tsukyko <zoeamat.pro@gmail.com>
2022-06-21 16:58:08 +0200

    Fix: rename env variables for pusher client

--- pusher-client.ts
+++ pusher-client.ts
@@ -1,10 +1,10 @@
 import Pusher from 'pusher';
 
 const pusher = new Pusher({
-  appId: process.env.APP_ID || '',
-  key: process.env.KEY || '',
-  secret: process.env.SECRET || '',
-  cluster: process.env.CLUSTER || '',
+  appId: process.env.PUSHER_APP_ID || '',
+  key: process.env.PUSHER_KEY || '',
+  secret: process.env.PUSHER_SECRET || '',
+  cluster: process.env.PUSHER_CLUSTER || '',
   useTLS: true,
 });
 
