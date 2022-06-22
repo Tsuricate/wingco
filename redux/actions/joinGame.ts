2697a9f7f67d4d7f5ff2cd7ce2343de712f20ece
Tsukyko <zoeamat.pro@gmail.com>
2022-06-22 17:31:44 +0200

    Chore: new action type & creator to handle answer to join game request

--- redux/actions/joinGame.ts
+++ redux/actions/joinGame.ts
@@ -1,5 +1,6 @@
 export const UPDATE_JOIN_GAME_SLUG = 'UPDATE_JOIN_GAME_SLUG';
 export const JOIN_GAME_REQUEST = 'JOIN_GAME_REQUEST';
+export const ANSWER_JOIN_REQUEST = 'ANSWER_JOIN_REQUEST';
 
 export const updateJoinGameSlug = (value: string, name: string) => ({
   type: UPDATE_JOIN_GAME_SLUG,
@@ -9,4 +10,11 @@ export const updateJoinGameSlug = (value: string, name: string) => ({
 
 export const joinGameRequest = () => ({
   type: JOIN_GAME_REQUEST,
+});
+
+export const answerJoinRequest = (playerId: string, isAccepted: boolean, gameSlug: string) => ({
+  type: ANSWER_JOIN_REQUEST,
+  playerId,
+  isAccepted,
+  gameSlug,
 });
