11d628a0f287a17bf3505235a896eb71b5a306e8
Tsukyko <zoeamat.pro@gmail.com>
2022-07-01 19:36:40 +0200

    Chore: create action creator & type for handling players in queue

--- redux/actions/joinGame.ts
+++ redux/actions/joinGame.ts
@@ -1,6 +1,10 @@
+import { Player } from '../../models/players';
+
 export const UPDATE_JOIN_GAME_SLUG = 'UPDATE_JOIN_GAME_SLUG';
 export const JOIN_GAME_REQUEST = 'JOIN_GAME_REQUEST';
 export const ANSWER_JOIN_REQUEST = 'ANSWER_JOIN_REQUEST';
+export const ADD_PLAYER_IN_QUEUE = 'ADD_PLAYER_IN_QUEUE';
+export const DELETE_PLAYER_IN_QUEUE = 'DELETE_PLAYER_IN_QUEUE';
 
 export const updateJoinGameSlug = (value: string, name: string) => ({
   type: UPDATE_JOIN_GAME_SLUG,
@@ -17,4 +21,14 @@ export const answerJoinRequest = (playerId: string, isAccepted: boolean, gameSlu
   playerId,
   isAccepted,
   gameSlug,
+});
+
+export const addPlayerInQueue = (player: Player) => ({
+  type: ADD_PLAYER_IN_QUEUE,
+  player,
+});
+
+export const deletePlayerInQueue = (playerId: Player['id']) => ({
+  type: DELETE_PLAYER_IN_QUEUE,
+  playerId,
 });
