d0e7567540eaacc947c86fb3a6ab532c675e6951
Tsukyko <zoeamat.pro@gmail.com>
2022-07-01 19:39:09 +0200

    Feature: move players queue to state + handle adding and delelting player from queue

--- redux/reducers/joinGameReducer.ts
+++ redux/reducers/joinGameReducer.ts
@@ -1,12 +1,15 @@
 import { AnyAction } from 'redux';
-import { UPDATE_JOIN_GAME_SLUG } from '../actions/joinGame';
+import { Player } from '../../models/players';
+import { ADD_PLAYER_IN_QUEUE, DELETE_PLAYER_IN_QUEUE, UPDATE_JOIN_GAME_SLUG } from '../actions/joinGame';
 
 interface joinGameReducerProps {
   gameSlug: string;
+  playersInQueue: Array<Player>;
 }
 
 const initialState: joinGameReducerProps = {
   gameSlug: '',
+  playersInQueue: [],
 };
 
 const joinGameReducer = (state = initialState, action: AnyAction) => {
@@ -15,6 +18,21 @@ const joinGameReducer = (state = initialState, action: AnyAction) => {
       return {
         ...state,
         [action.name]: action.value,
+      };
+    }
+
+    case ADD_PLAYER_IN_QUEUE: {
+      return {
+        ...state,
+        playersInQueue: [...state.playersInQueue, { ...action.player }],
+      };
+    }
+
+    case DELETE_PLAYER_IN_QUEUE: {
+      const newPlayerList = state.playersInQueue.filter((player) => player.id !== action.playerId);
+      return {
+        ...state,
+        playersInQueue: newPlayerList,
       };
     }
     default:
